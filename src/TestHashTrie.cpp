3f41946bba0e329150ed0c89a7d3ea41953ce223
Eric Lombrozo <elombrozo@gmail.com>
2015-05-13 17:15:04 -0700

    Multiple insertions or removals from test tool.

--- src/TestHashTrie.cpp
+++ src/TestHashTrie.cpp
@@ -20,13 +20,13 @@ int main(int argc, char* argv[])
     {
         MMRTree<LevelDBModel> tree("TestTree");
 
-        if (argc > 1)
+        for (int i = 1; i < argc; i++)
         {
-            if (string(argv[1]) == "-") { tree.removeItem(); }
-            else                        { tree.appendItem(uchar_vector(argv[1])); }
-            tree.commit();
+            if (string(argv[i]) == "-") { tree.removeItem(); }
+            else                        { tree.appendItem(uchar_vector(argv[i])); }
         }
 
+        tree.commit();
         cout << tree.json() << endl;
     }
     catch (const exception& e)
