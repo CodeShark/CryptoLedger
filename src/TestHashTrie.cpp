0639b2bf852a27927a7bef6a713d7c4c0396dbed
Eric Lombrozo <elombrozo@gmail.com>
2015-05-13 22:23:37 -0700

    MMRTree::path() test

--- src/TestHashTrie.cpp
+++ src/TestHashTrie.cpp
@@ -6,27 +6,38 @@
 using namespace HashTrie;
 using namespace std;
 
+void showPath(const vector<bool>& path)
+{
+    for (auto b: path) { cout << (b ? "R" : "L"); }
+    cout << endl;
+}
+
 int main(int argc, char* argv[])
 {
-/*
-    if (argc != 2)
-    {
-        cerr << "# Usage: " << argv[0] << " <hex data>" << endl;
-        return -1;
-    }
-*/
-
     try
     {
         MMRTree<LevelDBModel> tree("TestTree");
 
-        for (int i = 1; i < argc; i++)
+        if (argc > 1)
         {
-            if (string(argv[i]) == "-") { tree.removeItem(); }
-            else                        { tree.appendItem(uchar_vector(argv[i])); }
+            if (string(argv[1]) == "p")
+            {
+                if (argc != 3) throw runtime_error("No item index specified for option p.");
+                uint64_t i = strtoull(argv[2], NULL, 0);
+                vector<bool> path = tree.path(i);
+                showPath(path);
+                return 0;
+            }
+            
+            for (int i = 1; i < argc; i++)
+            {
+                if (string(argv[i]) == "-") { tree.removeItem(); }
+                else                        { tree.appendItem(uchar_vector(argv[i])); }
+            }
+
+            tree.commit();
         }
 
-        tree.commit();
         cout << tree.json() << endl;
     }
     catch (const exception& e)
